26aac421d06db5b7f8df3f23e43da5086c738703
matterisnanto <rachmat.trisnanto186@gmail.com>
2025-03-01 23:36:19 +0700

    ubah pos

--- database/migrations/2025_03_01_023944_create_pos_transactions_table.php
+++ database/migrations/2025_03_01_023944_create_pos_transactions_table.php
@@ -10,7 +10,7 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('p_o_s_transactions', function (Blueprint $table) {
+        Schema::create('pos_transactions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
