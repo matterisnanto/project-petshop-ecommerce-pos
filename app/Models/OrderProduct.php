16cca2fcb056f056cba688ab307d2e2a7d7077e7
dessvira <desvira2903@gmail.com>
2025-03-06 15:15:39 +0700

    mengupdate stock produk dengan observer

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\OrderProductObserver;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
+#[ObservedBy([OrderProductObserver::class])]
 class OrderProduct extends Model
 {
     use HasFactory;
@@ -14,12 +18,12 @@ class OrderProduct extends Model
     protected $fillable = ['pos_transaction_id', 'product_id', 'quantity', 'unit_price'];
 
 
-    public function posTransaction()
+    public function posTransaction(): BelongsTo
     {
         return $this->belongsTo(PosTransaction::class, 'pos_transaction_id');
     }
         
-    public function product()
+    public function product():BelongsTo 
     {
         return $this->belongsTo(Product::class, 'product_id');
     }
